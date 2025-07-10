fd48cc323c34efb7f7826070e14fe3cb0f32d884
Aditya-a404a <aditya789arora@gmail.com>
2025-07-10 14:02:02 +0530

    Changed ServerAction to call inhouse api

--- app/api/run/route.ts
+++ app/api/run/route.ts
@@ -3,49 +3,24 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
   try {
-    const { language, code } = await req.json();
+    const { language, code , input } = await req.json();
 
-    // Step 1: Get supported runtimes
-    const runtimesRes = await fetch('https://emkc.org/api/v2/piston/runtimes');
-    const runtimes = await runtimesRes.json();
-
-    // Step 2: Find the latest version for the language
-    const runtime = runtimes.find((r: any) => r.language === language);
-    if (!runtime) {
-      return NextResponse.json({ error: "Unsupported language" }, {
-        status: 400,
-        headers: corsHeaders,
-      });
-    }
-
-    const version = runtime.version;
-
-    // Step 3: Send execution request
-    const pistonRes = await fetch('https://emkc.org/api/v2/piston/execute', {
+    const pistonRes = await fetch('http://16.171.171.154:8000/run', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         language,
-        version,
-        files: [
-          {
-            name: `main.${language}`,
-            content: code,
-          },
-        ],
-        // stdin: "Hello from stdin!",
-        run_timeout: 3000,
-        compile_timeout: 10000,
-      }),
-    });
+        code,
+        input,
+    })});
 
     const result = await pistonRes.json();
 
     return NextResponse.json(
       {
-        stdout: result.run.stdout,
-        stderr: result.run.stderr,
-        code: result.run.code,
+        output: result.output,
+        error: result.error,
+        execution_time: result.execution_time,
       },
       {
         status: 200,
